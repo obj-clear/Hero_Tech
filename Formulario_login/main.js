57c45a5f47b857f8cfc8760cccee52004358a006
obj-clear <davidgh_20@hotmail.com>
2024-11-21 19:22:18 -0500

    corrigiendo 8

--- Formulario_login/main.js
+++ Formulario_login/main.js
@@ -3,16 +3,17 @@
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
   import { getAuth ,GoogleAuthProvider ,signInWithPopup } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
   
+  // Configuración de Firebase
   const firebaseConfig = {
-    apiKey: "AIzaSyAsBcvgdrqFM9dCS4NytDKjdYSwK_gqaGE",
-    authDomain: "login-8ff6e.firebaseapp.com",
-    projectId: "login-8ff6e",
-    storageBucket: "login-8ff6e.firebasestorage.app",
-    messagingSenderId: "46000541427",
-    appId: "1:46000541427:web:87846aa32c6932168ef186"
-  };
+    apiKey: "TU_API_KEY",
+    authDomain: "TU_AUTH_DOMAIN",
+    projectId: "TU_PROJECT_ID",
+    storageBucket: "TU_STORAGE_BUCKET",
+    messagingSenderId: "TU_MESSAGING_SENDER_ID",
+    appId: "TU_APP_ID"
+};
 
-// Initialize Firebase
+// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 auth.languageCode = 'en';
@@ -26,15 +27,19 @@ googleLogin.addEventListener("click", function(){
         const credential = GoogleAuthProvider.credentialFromResult(result);
         const user = result.user;
         console.log(user);
-        
-        // Guardamos el estado de autenticación exitosa en sessionStorage
-        sessionStorage.setItem('authSuccess', 'true');
-        
-        // Redirigir a index.html
-        window.location.href = "../index.html";
+
+        // Redirigir a la página de inicio
+        window.location.href = "index.html";
     }).catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        console.error(errorMessage);
     });
+});
+
+// Mostrar el toast después de la autenticación exitosa
+window.addEventListener('load', () => {
+    if (window.location.pathname === '/index.html') {
+        const toast = new bootstrap.Toast(document.getElementById('login-toast'));
+        toast.show();
+    }
 });
