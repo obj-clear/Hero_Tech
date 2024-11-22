cf11eaa05a1b37281df991fc9b6829af8afce6a3
obj-clear <davidgh_20@hotmail.com>
2024-11-21 19:47:35 -0500

    corrigiendo 12

--- Formulario_login/main.js
+++ Formulario_login/main.js
@@ -1,41 +1,39 @@
-
- 
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
   import { getAuth ,GoogleAuthProvider ,signInWithPopup } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
   
-  // Configuración de Firebase
   const firebaseConfig = {
-    apiKey: "TU_API_KEY",
-    authDomain: "TU_AUTH_DOMAIN",
-    projectId: "TU_PROJECT_ID",
-    storageBucket: "TU_STORAGE_BUCKET",
-    messagingSenderId: "TU_MESSAGING_SENDER_ID",
-    appId: "TU_APP_ID"
-};
+    apiKey: "AIzaSyAsBcvgdrqFM9dCS4NytDKjdYSwK_gqaGE",
+    authDomain: "login-8ff6e.firebaseapp.com",
+    projectId: "login-8ff6e",
+    storageBucket: "login-8ff6e.firebasestorage.app",
+    messagingSenderId: "46000541427",
+    appId: "1:46000541427:web:87846aa32c6932168ef186"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-auth.languageCode = 'en';
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  auth.languagecode = 'en'
 
-const provider = new GoogleAuthProvider();
+  const provider = new GoogleAuthProvider();
 
-const googleLogin = document.getElementById("google-login-btn");
-googleLogin.addEventListener("click", function(){
+  const googleLogin = document.getElementById("google-login-btn");
+  googleLogin.addEventListener("click", function(){
     signInWithPopup(auth, provider)
-    .then((result) => {
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const user = result.user;
-        console.log(user);
-        
-        // Guardamos el estado de autenticación exitosa en sessionStorage
-        sessionStorage.setItem('authSuccess', 'true');
-        
-        // Redirigir a index.html
-        window.location.href = "../index.html";
-    }).catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.error(errorMessage);
-    });
-});
+  .then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const user = result.user;
+    console.log(user);
+    window.location.href = "../index.html";
+
+  }).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    
+  });
+
+
+
+
+
+  })
