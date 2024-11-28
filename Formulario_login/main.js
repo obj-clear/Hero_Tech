7c111da98c50bcc65de88eb8e7997e165744aece
obj-clear <davidgh_20@hotmail.com>
2024-11-28 02:05:02 -0500

    ok

--- Formulario_login/main.js
+++ Formulario_login/main.js
@@ -10,7 +10,8 @@
     appId: "1:46000541427:web:87846aa32c6932168ef186"
   };
 
-  // Inicializando Firebase sasafasfasf
+  // Inicializando Firebase
+  //xdddd
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
   auth.languagecode = 'en'
